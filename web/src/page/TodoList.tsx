8442c432e1543088ce401ea940a1e8f9c9e52837
mun8123 <mun8123@gmail.com>
2022-10-19 00:02:46 +0900

    feat: 투두리스트 데이터 요청 후 초기 상태 세팅

--- web/src/page/TodoList.tsx
+++ web/src/page/TodoList.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
+import { TodoContext } from "../context";
+import { TodoItem } from "../context/TodoProvider";
+import { useFetch } from "../hook";
+import { BASE_URL } from "../constant";
+import { getLoginToken } from "../util/login";
 
 function TodoList() {
-  return <div>투두리스트</div>;
+  const { setInitialTodos } = useContext(TodoContext);
+  const loading = useFetch<TodoItem[]>(
+    `${BASE_URL}/todos`,
+    {
+      headers: {
+        Authorization: getLoginToken(),
+      },
+    },
+    setInitialTodos
+  );
+
+  return (
+    <>
+      <div>{loading ? "로딩중" : "투두리스트"}</div>;
+    </>
+  );
 }
 
 export default TodoList;
