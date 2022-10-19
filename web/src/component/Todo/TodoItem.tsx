823ec97ff669ef66f151f473b8b6b69e78ea4574
mun8123 <mun8123@gmail.com>
2022-10-20 02:52:43 +0900

    feat: 투두 아이템 수정하기 로직 추가

--- web/src/component/Todo/TodoItem.tsx
+++ web/src/component/Todo/TodoItem.tsx
@@ -1,16 +1,46 @@
-import { TodoItem as TodoItemType } from "../../type";
 import { Button } from "../../styled/button";
+import { TodoItem as TodoItemType } from "../../type";
+import { useFetch } from "../../hook";
+import { BASE_URL } from "../../constant";
+import { buildOption } from "../../util";
+import { getLoginToken } from "../../util/login";
 
 interface TodoItemProps {
   todo: TodoItemType;
 }
 
 function TodoItem({ todo }: TodoItemProps) {
+  const { post } = useFetch({ baseUrl: BASE_URL });
+
+  const onClick = (
+    e:
+      | React.MouseEvent<HTMLButtonElement>
+      | React.KeyboardEvent<HTMLButtonElement>
+  ) => {
+    const target = e.target as HTMLButtonElement;
+    const liElement = target.closest(".todo-item") as HTMLLIElement;
+    const todoId = liElement.id;
+
+    if (target.id === "edit") {
+      post({
+        endPoint: `/todos/${todoId}`,
+        options: buildOption(getLoginToken(), "PUT", {
+          title: "1234",
+          content: "qwer",
+        }),
+      });
+    }
+  };
+
   return (
-    <li>
+    <li id={todo.id} className={"todo-item"}>
       <p>{todo.title}</p>
-      <Button>수정</Button>
-      <Button>삭제</Button>
+      <Button type="submit" id={"edit"} onClick={onClick} onKeyDown={onClick}>
+        수정
+      </Button>
+      <Button type="submit" id={"delete"} onClick={onClick} onKeyDown={onClick}>
+        삭제
+      </Button>
     </li>
   );
 }
